2228f837cf80acdaf91eb4016636461c89b38aae
weslinARS <weslin.rodriguez@est.ulsa.edu.ni>
2025-08-01 15:03:14 -0600

    feat :  tipos de argumentos para repositorio user agregados.

--- src/shared/common/prisma-args.ts
+++ src/shared/common/prisma-args.ts
@@ -1,3 +1,5 @@
+import { Prisma } from "~gen-prisma/index";
+
 export type CommonAgrs = {
 	includeDeleted?: boolean;
 	isSearchMode?: boolean;
@@ -9,4 +11,22 @@ export type CommonAgrs = {
 export type QueryManyWithCount<T extends object> = {
 	records: T[];
 	count: number;
+};
+
+export interface PrismaRepositoryResponse {
+	success: boolean;
+
+	data?: string;
+}
+// user
+
+export type findOneUserArgs = CommonAgrs & {
+	include?: Prisma.UserInclude;
+	select?: Prisma.UserSelect;
+	omit?: Prisma.UserOmit;
+};
+
+export type findManyUserArgs = findOneUserArgs & {
+	where?: Prisma.UserWhereInput;
+	orderBy?: Prisma.UserOrderByWithRelationInput;
 };
