9c4c000f493741baca71bf6b35ab944fb1755c6e
weslinARS <weslin.rodriguez@est.ulsa.edu.ni>
2025-08-10 12:56:52 -0600

    feat: redefinir el DTO de actualización de usuario con validaciones y campos opcionales

--- src/modules/users/dto/update-user.dto.ts
+++ src/modules/users/dto/update-user.dto.ts
@@ -1,3 +1,38 @@
-import { SignUpUserDto } from "src/modules/auth/dtos/sign-up-user.dto";
+import { $Enums } from "@prisma/client";
+import { IsEmail, IsEnum, IsOptional, IsString } from "class-validator";
 
-export class UpdateUserDto implements Partial<SignUpUserDto> {}
+export class UpdateUserDto {
+	@IsOptional()
+	@IsString({
+		message: "El nombre es un campo de texto",
+	})
+	firstName?: string;
+
+	@IsOptional()
+	@IsString({
+		message: "El apellido es un campo de texto",
+	})
+	lastName?: string;
+
+	@IsOptional()
+	@IsEnum($Enums.UserRole, {
+		message: "El rol debe ser uno de los valores permitidos",
+	})
+	role?: $Enums.UserRole;
+
+	@IsOptional()
+	@IsEmail(
+		{},
+		{
+			message:
+				"El correo electrónico debe ser un correo electrónico válido",
+		},
+	)
+	email?: string;
+
+	@IsOptional()
+	@IsString({
+		message: "La contraseña es un campo de texto",
+	})
+	password?: string;
+}
